f1235ac89cb70610ce2f20b6bf0ccaf21584903c
Matteo Concas <matteo.concas@cern.ch>
2024-03-07 08:22:05 +0100

    Update scripts

--- common-config.sh
+++ common-config.sh
@@ -6,13 +6,13 @@ killall o2-its-reco-workflow -s 9;
 killall o2-ctf-reader-workflow -s 9;
 
 ITS_RECOPAR="--configKeyValues "
-MAXTF=-1;
+MAXTF=1;
 GLOSET="--shm-segment-size 32000000000 --timeframes-rate-limit 3";
 
 # Define ConfigKeyValues
 ITS_CLUSTERPAR="ITSClustererParam.maxBCDiffToMaskBias=-10;ITSClustererParam.maxBCDiffToSquashBias=10"
 FASTMULTPAR="fastMultConfig.cutMultClusLow=-1;fastMultConfig.cutMultClusHigh=-1;fastMultConfig.cutMultVtxHigh=-1"
-ITS_VTXPAR="ITSVertexerParam.phiCut=0.005;ITSVertexerParam.clusterContributorsCut=16;ITSVertexerParam.tanLambdaCut=0.002;ITSVertexerParam.lowMultBeamDistCut=0;ITSCATrackerParam.useFastMaterial=true"
+ITS_VTXPAR="ITSVertexerParam.phiCut=0.005;ITSVertexerParam.clusterContributorsCut=16;ITSVertexerParam.tanLambdaCut=0.002;ITSVertexerParam.lowMultBeamDistCut=0;ITSCATrackerParam.saveTimeBenchmarks=true"
 
 # Assemble parameters
 ITS_RECOPAR+="$ITS_CLUSTERPAR;$FASTMULTPAR;$ITS_VTXPAR;"
--- cuda-manual.sh
+++ cuda-manual.sh
@@ -8,12 +8,12 @@ source common-config.sh
 # export ALICEO2_CCDB_LOCALCACHE=$PWD/locCCDB \
 # export IGNORE_VALIDITYCHECK_OF_CCDB_LOCALCACHE=1 \
 # export USE_CHECKER=
-
+# export USE_CHECKER=1
 export GPUMEMSIZE=10000000000
 ITS_RECOPAR+="GPU_proc.forceMemoryPoolSize=10000000000;GPU_global.deviceType=CUDA"
 export GPU_DEVICE=2
 o2-ctf-reader-workflow ${GLOSET}  --severity error --max-tf=${MAXTF:-1} --ctf-input data.lst --onlyDet ITS  --allow-missing-detectors  --its-digits --configKeyValues ";;" | \
-o2-its-reco-workflow ${GLOSET} --trackerCA --tracking-mode async --disable-mc --digits-from-upstream --use-gpu-workflow --gpu-device=${GPU_DEVICE} ${ITS_RECOPAR} -b --run  2>&1 > cuda.log
+${USE_CHECKER:+compute-sanitizer --tool memcheck --target-processes=all --report-api-errors=all} o2-its-reco-workflow ${GLOSET} --trackerCA --tracking-mode async --disable-mc --digits-from-upstream --use-gpu-workflow --gpu-device=${GPU_DEVICE} ${ITS_RECOPAR} -b --run  2>&1 | tee cuda.log
 
 mv its_time_benchmarks.txt its_time_benchmarks_cuda.txt
 mv o2trac_its.root o2trac_its_cuda.root
