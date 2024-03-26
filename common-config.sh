dc32202d981b3362542a757a350f3f2b4a1792ca
Matteo Concas <matteo.concas@cern.ch>
2024-03-26 13:53:36 +0100

    Add pp cuts as default

--- common-config.sh
+++ common-config.sh
@@ -12,7 +12,12 @@ GLOSET="--shm-segment-size 32000000000 --timeframes-rate-limit 3";
 # Define ConfigKeyValues
 ITS_CLUSTERPAR="ITSClustererParam.maxBCDiffToMaskBias=-10;ITSClustererParam.maxBCDiffToSquashBias=10"
 FASTMULTPAR="fastMultConfig.cutMultClusLow=-1;fastMultConfig.cutMultClusHigh=-1;fastMultConfig.cutMultVtxHigh=-1"
+
+# Cuts for PbPb specificially
 ITS_VTXPAR="ITSVertexerParam.phiCut=0.005;ITSVertexerParam.clusterContributorsCut=16;ITSVertexerParam.tanLambdaCut=0.002;ITSVertexerParam.lowMultBeamDistCut=0;ITSCATrackerParam.saveTimeBenchmarks=true"
+
+# Cuts for pp specifically
+ITS_VTXPAR="ITSCATrackerParam.saveTimeBenchmarks=true;ITSVertexerParam.phiCut=0.5;ITSVertexerParam.clusterContributorsCut=3;ITSVertexerParam.tanLambdaCut=0.2;;ITSCATrackerParam.saveTimeBenchmarks=true"
 
 # Assemble parameters
 ITS_RECOPAR+="$ITS_CLUSTERPAR;$FASTMULTPAR;$ITS_VTXPAR;"
