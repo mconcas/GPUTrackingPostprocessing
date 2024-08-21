11fb19cffb29b2082faf9b0f62255df73fda88a7
Matteo Concas <matteo.concas@cern.ch>
2024-08-21 18:51:59 +0200

    Simplify some paramters

--- common-config_pbpb.sh
+++ common-config_pbpb.sh
@@ -5,9 +5,10 @@ rm -f /dev/shm/*fmq*;
 killall o2-its-reco-workflow -s 9;
 killall o2-ctf-reader-workflow -s 9;
 
-MODE=async
+MODE=sync
+MODE_NUM=$((MODE == "sync" ? 0 : 1))
 ITS_RECOPAR="--configKeyValues "
-MAXTF=10;
+MAXTF=1;
 GLOSET="--shm-segment-size 16000000000";
 
 # Define ConfigKeyValues
@@ -15,14 +16,14 @@ ITS_CLUSTERPAR=";ITSClustererParam.maxBCDiffToMaskBias=-10;ITSClustererParam.max
 FASTMULTPAR=";fastMultConfig.cutMultClusLow=-1;fastMultConfig.cutMultClusHigh=-1;fastMultConfig.cutMultVtxHigh=-1;"
 
 # Cuts for PbPb specificially
-ITS_TRKPAR="ITSCATrackerParam.nThreads=20;ITSVertexerParam.nThreads=20;ITSVertexerParam.phiCut=0.005;ITSVertexerParam.clusterContributorsCut=16;ITSVertexerParam.tanLambdaCut=0.002;ITSVertexerParam.lowMultBeamDistCut=0;ITSCATrackerParam.saveTimeBenchmarks=true;ITSCATrackerParam.trackingMode=$((MODE == "async"))"
+ITS_TRKPAR="ITSCATrackerParam.nThreads=20;ITSVertexerParam.nThreads=20;ITSVertexerParam.phiCut=0.005;ITSVertexerParam.clusterContributorsCut=16;ITSVertexerParam.tanLambdaCut=0.002;ITSVertexerParam.lowMultBeamDistCut=0;ITSCATrackerParam.saveTimeBenchmarks=true;ITSCATrackerParam.trackingMode=${MODE_NUM}"
 
 # Sync or async
 # --ccdb-meanvertex-seed
 ITS_TRACKING_MODE="--tracking-mode ${MODE}"
 
 # GPU workflow specific config to run TPC GPU
-GPU_WORKFLOW_PARS=";ITSCATrackerParam.trackingMode=$((MODE == "async"));GPU_global.deviceType=HIP;GPU_proc.forceMemoryPoolSize=10000000000;GPU_proc.forceHostMemoryPoolSize=1073741824;"
+GPU_WORKFLOW_PARS=";GPU_global.deviceType=HIP;GPU_proc.forceMemoryPoolSize=10000000000;GPU_proc.forceHostMemoryPoolSize=1073741824;"
 
 # Assemble parameters
 ITS_RECOPAR+=";$ITS_CLUSTERPAR;$FASTMULTPAR;$ITS_TRKPAR;"
--- hip-manual.sh
+++ hip-manual.sh
@@ -9,7 +9,7 @@ ITS_RECOPAR+="GPU_proc.forceMemoryPoolSize=10000000000;GPU_global.deviceType=HIP
 export GPU_DEVICE=3
 
 o2-ctf-reader-workflow ${GLOSET} --severity error --max-tf=${MAXTF:-1} --ctf-input data.lst --onlyDet ITS --allow-missing-detectors --its-digits --configKeyValues ";;" |
-    o2-its-reco-workflow ${GLOSET} --trackerCA ${ITS_TRACKING_MODE} --disable-mc --digits-from-upstream --use-gpu-workflow --gpu-device=${GPU_DEVICE} ${ITS_RECOPAR} -b --run 2>&1 | sed 's/\[[^]]*:[^]]*\]//g' | tee hip.log
+    o2-its-reco-workflow ${GLOSET} --trackerCA --tracking-mode sync --disable-mc --digits-from-upstream --use-gpu-workflow --gpu-device=${GPU_DEVICE} ${ITS_RECOPAR} -b --run 2>&1 | sed 's/\[[^]]*:[^]]*\]//g' | tee hip.log
 
 mv its_time_benchmarks.txt its_time_benchmarks_hip.txt
 mv o2trac_its.root o2trac_its_hip.root
